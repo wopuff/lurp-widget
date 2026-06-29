272a32c37049073bdc10ab5cad2ee8e1e4cbc3ce
Arvid <arvid.renestam@gmail.com>
2026-06-29 12:23:39 +0200

    feat: add logger configuration and add description to example project

--- lib/src/config/logger.dart
+++ lib/src/config/logger.dart
@@ -1,59 +1,64 @@
 import 'package:flutter/foundation.dart';
-import 'package:logger/logger.dart';
 import 'dart:developer' as developer;
 
-final Logger logger = Logger(
-  printer: CustomLogPrinter(),
-  output: CustomLogOutput(),
-);
+enum LurpLogLevel {
+  trace(0),
+  debug(500),
+  info(800),
+  warning(900),
+  error(1000),
+  fatal(2000);
 
-class CustomLogPrinter extends LogPrinter {
-  final LogPrinter _realPrinter = PrettyPrinter(
-    colors: true,
-    lineLength: 5,
-    dateTimeFormat: DateTimeFormat.none,
-    printEmojis: true,
-  );
-
-  @override
-  List<String> log(LogEvent event) {
-    return _realPrinter.log(event);
-  }
+  const LurpLogLevel(this.value);
+  final int value;
 }
 
-class CustomLogOutput extends LogOutput {
-  @override
-  void output(OutputEvent event) {
-    final messageBlock = event.lines.join('\n');
+class LurpLogger {
+  const LurpLogger();
 
+  void i(
+    dynamic message, {
+    Object? error,
+    StackTrace? stackTrace,
+  }) {
+    _log(LurpLogLevel.info, message, error, stackTrace);
+  }
+
+  void e(
+    dynamic message, {
+    Object? error,
+    StackTrace? stackTrace,
+  }) {
+    _log(LurpLogLevel.error, message, error, stackTrace);
+  }
+
+  void _log(LurpLogLevel level, dynamic message, Object? error, StackTrace? stackTrace) {
+    final timeStr = DateTime.now().toIso8601String().substring(11, 19);
+    final levelStr = level.name.toUpperCase();
+    
+    final StringBuffer buffer = StringBuffer();
+    buffer.write('[$timeStr] $levelStr: $message');
+    if (error != null) {
+      buffer.write('\nError: $error');
+    }
+    if (stackTrace != null) {
+      buffer.write('\n$stackTrace');
+    }
+
+    final msg = buffer.toString();
     developer.log(
-      messageBlock,
+      msg,
       name: 'lurp_poll',
-      level: _mapLogLevel(event.level),
+      level: level.value,
+      error: error,
+      stackTrace: stackTrace,
     );
 
     if (kIsWeb) {
       // ignore: avoid_print
-      print(messageBlock);
-    }
-  }
-
-  int _mapLogLevel(Level level) {
-    switch (level) {
-      case Level.trace:
-        return 0;
-      case Level.debug:
-        return 500;
-      case Level.info:
-        return 800;
-      case Level.warning:
-        return 900;
-      case Level.error:
-        return 1000;
-      case Level.fatal:
-        return 2000;
-      default:
-        return 0;
+      print(msg);
     }
   }
 }
+
+final logger = LurpLogger();
